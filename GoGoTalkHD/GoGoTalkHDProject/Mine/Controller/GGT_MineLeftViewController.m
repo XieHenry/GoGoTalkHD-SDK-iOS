3d425e1dd8d45d074633f9eee9c341aa0735961b
XieHenry <291964988@qq.com>
2017-07-07 16:06:47 +0800

    更改《我的》bug。。。

--- GoGoTalkHD/GoGoTalkHDProject/Mine/Controller/GGT_MineLeftViewController.m
+++ GoGoTalkHD/GoGoTalkHDProject/Mine/Controller/GGT_MineLeftViewController.m
@@ -82,7 +82,6 @@ static BOOL isShowTestReportVc;
     [self getLoadData];
     
 
-    
 }
 
 #pragma mark 获取左边的名称和icon
@@ -100,22 +99,7 @@ static BOOL isShowTestReportVc;
 
         [_tableView reloadData];
         
-    } failure:^(NSError *error) {
-        [MBProgressHUD showMessage:error.userInfo[@"msg"] toView:self.view];
-        
-    }];
-}
-
-
-#pragma mark 获取网络请求，添加到view上
-- (void)getLoadData {
-    
-    [[BaseService share] sendGetRequestWithPath:URL_GetLessonStatistics token:YES viewController:self showMBProgress:NO success:^(id responseObject) {
-        
-        _model = [GGT_MineLeftModel yy_modelWithDictionary:responseObject[@"data"]];
-        [_headerView getResultModel:_model];
-        [_tableView reloadData];
-        
+        //先刷新数据，再选中cell
         GGT_Singleton *sin = [GGT_Singleton sharedSingleton];
         if (sin.isAuditStatus == YES ) {
             if (isShowTestReportVc == YES) {
@@ -133,6 +117,22 @@ static BOOL isShowTestReportVc;
             }
         }
         
+        
+        
+    } failure:^(NSError *error) {
+        [MBProgressHUD showMessage:error.userInfo[@"msg"] toView:self.view];
+        
+    }];
+}
+
+
+#pragma mark 获取网络请求，添加到view上
+- (void)getLoadData {
+    
+    [[BaseService share] sendGetRequestWithPath:URL_GetLessonStatistics token:YES viewController:self showMBProgress:NO success:^(id responseObject) {
+        
+        _model = [GGT_MineLeftModel yy_modelWithDictionary:responseObject[@"data"]];
+        [_headerView getResultModel:_model];
 
     } failure:^(NSError *error) {
         [MBProgressHUD showMessage:error.userInfo[@"msg"] toView:self.view];
