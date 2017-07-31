1508826d003731d6df1411693598cc721b581f9f
XieHenry <291964988@qq.com>
2017-07-31 10:54:18 +0800

    上线版

--- GoGoTalkHD/GoGoTalkHDProject/Mine/Controller/GGT_SettingViewController.m
+++ GoGoTalkHD/GoGoTalkHDProject/Mine/Controller/GGT_SettingViewController.m
@@ -120,6 +120,23 @@
         cell.contentLabel.text = [NSString stringWithFormat:@"%.2fM",[self folderSize]];
         
     } else if (indexPath.row == 3) {
+
+        //更新坐标
+        cell.rightImgView.hidden = YES;
+        [cell.contentLabel mas_updateConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(cell.leftTitleLabel.mas_right).with.offset(LineX(15));
+            make.right.equalTo(cell.rightImgView.mas_left).with.offset(-LineX(20));
+            make.centerY.equalTo(cell.contentView.mas_centerY);
+            make.height.mas_offset(LineH(22));
+        }];
+        
+        
+        [cell.rightImgView mas_updateConstraints:^(MASConstraintMaker *make) {
+            make.right.equalTo(cell.contentView.mas_right).with.offset(-LineX(0));
+            make.centerY.equalTo(cell.contentView.mas_centerY);
+            make.size.mas_offset(CGSizeMake(LineW(0), LineH(0)));
+        }];
+        
         
         NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
         // app版本
